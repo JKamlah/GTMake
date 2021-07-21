b4c579eb473f20ef9f0e0027a1a4730306d20027
JKamlah <jan.kamlah@gmail.com>
2021-07-21 12:33:41 +0200

    Rename func, add cli and rework adding deleted files to git.

--- delete_files.py
+++ delete_files.py
@@ -6,10 +6,15 @@ import click
 from tqdm import tqdm
 
 
-@click.command()
+@click.group()
+def cli():
+    pass
+
+@cli.command()
+@click.argument('gtpath', nargs=1, type=click.Path(exists=True))
 @click.argument('list-of-files', nargs=1, type=click.Path(exists=True))
 @click.option('-v', '--verbose', default=False, is_flag=True, help='Print more process information')
-def delete_files_with_list(list_of_files, verbose):
+def files_via_list(gtpath, list_of_files, verbose):
     """ Deletes files provided by an list."""
     with open(list_of_files, "r") as fin:
         for fname in tqdm(fin.readlines()):
@@ -17,20 +22,21 @@ def delete_files_with_list(list_of_files, verbose):
                 continue
             if verbose:
                 print(fname)
-            for delfname in glob.glob(f"./{fname.strip().split('.')[0]}*"):
+            for delfname in glob.glob(os.path.join(gtpath,fname.strip().split('.')[0]+"*")):
                 if verbose:
                     print(delfname)
                 os.remove(delfname)
 
 
-@click.command()
+@cli.command()
+@click.argument('gtpath', nargs=1, type=click.Path(exists=True))
 @click.option('-e', '--image-extension', default='png')
 @click.option('--text-extension', default='gt.txt')
 @click.option('-v', '--verbose', default=False, is_flag=True, help='Print more process information')
-def delete_files_without_textfiles(image_extension, text_extentsion, verbose):
+def files_without_textfiles(gtpath, image_extension, text_extension, verbose):
     """ Deletes image and json files if no equivalent textfile is in the path."""
-    images = set([img.split(".")[0] for img in glob.glob(f"*.{image_extension}")])
-    txt = set([txt.split(".")[0] for txt in glob.glob(f"*.{text_extentsion}")])
+    images = set([str(img.resolve()).rsplit(".", len(img.suffixes))[0] for img in gtpath.rglob(f"*.{image_extension}")])
+    txt = set([str(txt.resolve()).rsplit(".", len(txt.suffixes))[0] for txt in gtpath.rglob(f"*.{text_extension}")])
     for delname in tqdm(txt.difference(images)):
         fname = Path(delname)
         if verbose:
@@ -41,47 +47,49 @@ def delete_files_without_textfiles(image_extension, text_extentsion, verbose):
             os.remove(delfname)
 
 
-@click.command()
+@cli.command()
 @click.argument('gtpath', nargs=1, type=click.Path(exists=True))
 @click.option('-e', '--image-extension', default='png')
 @click.option('--text-extension', default='gt.txt')
 @click.option('-v', '--verbose', default=False, is_flag=True, help='Print more process information')
-def delete_gitrepo_files_without_textfiles(gtpath, image_extension, text_extentsion, verbose):
+def gitrepo_without_textfiles(gtpath, image_extension, text_extension, verbose):
     """ Deletes image and json files if no equivalent textfile is in the repo."""
     from git import Repo
-    repo = Repo(gtpath, search_parent_directories=True)
-    images = set([img.split(".")[0] for img in glob.glob(f"*.{image_extension}")])
-    txt = set([txt.split(".")[0] for txt in glob.glob(f"*.{text_extentsion}")])
-    for delname in tqdm(txt.difference(images)):
-        fname = Path(delname)
+    gtpath = Path(gtpath)
+    repo = Repo(str(gtpath.resolve()), search_parent_directories=True)
+    images = set([str(img.resolve()).rsplit(".", len(img.suffixes))[0] for img in gtpath.rglob(f"*.{image_extension}")])
+    txt = set([str(txt.resolve()).rsplit(".", len(txt.suffixes))[0] for txt in gtpath.rglob(f"*.{text_extension}")])
+    for fnames in tqdm(txt.difference(images)):
         if verbose:
-            print(f"All files starting with {str(fname.absolute()).split('.', 1)[0]} will be removed:")
-        for delfname in glob.glob(f"{str(fname.absolute()).split('.', 1)[0]}*"):
+            print(f"All files starting with {fnames.split('.', 1)[0]} will be removed:")
+        for del_fname in glob.glob(f"{fnames.split('.', 1)[0]}*"):
             if verbose:
-                print(delfname)
-            os.remove(delfname)
-            repo.index.add([delfname.resolve()])
-            repo.index.commit(f"DELETE {delname}")
+                print(del_fname)
+            os.remove(del_fname)
+            repo.index.remove(del_fname)
+            repo.index.commit(f"DELETE {del_fname}")
 
 
-@click.command()
+@cli.command()
 @click.argument('gtpath', nargs=1, type=click.Path(exists=True))
 @click.option('--text-extension', default='gt.txt')
 @click.option('-v', '--verbose', default=False, is_flag=True, help='Print more process information')
-def delete_gitrepo_files_with_empty_textfiles(gtpath, text_extentsion, verbose):
+def gitrepo_with_empty_textfiles(gtpath, text_extension, verbose):
     """ Deletes image and json files if the equivalent textfile is empty."""
     from git import Repo
     repo = Repo(gtpath, search_parent_directories=True)
-    txtfiles = set([txt.split(".")[0] for txt in glob.glob(f"*.{text_extentsion}")])
-    for delname in tqdm(txtfiles):
+    txt = set([str(txt.resolve()).rsplit(".", len(txt.suffixes))[0] for txt in gtpath.rglob(f"*.{text_extension}")])
+    for fnames in tqdm(txt):
         if os.stat("file").st_size != 0:
             continue
-        fname = Path(delname)
         if verbose:
-            print(f"All files starting with {str(fname.absolute()).split('.', 1)[0]} will be removed:")
-        for delfname in glob.glob(f"{str(fname.absolute()).split('.', 1)[0]}*"):
+            print(f"All files starting with {fnames.split('.', 1)[0]} will be removed:")
+        for del_fname in glob.glob(f"{fnames.split('.', 1)[0]}*"):
             if verbose:
-                print(delfname)
-            os.remove(delfname)
-            repo.index.add([delfname.resolve()])
-            repo.index.commit(f"DELETE {delname}")
+                print(del_fname)
+            os.remove(del_fname)
+            repo.index.remove(del_fname)
+            repo.index.commit(f"DELETE {del_fname}")
+
+if __name__=='__main__':
+    cli()
